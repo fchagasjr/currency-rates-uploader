ac4aae15c9265cfd8cd83eeb0f13d7fd43d2b0c1
Francisco das Chagas Silva Junior <fcsjr.chagas@gmail.com>
2025-01-25 20:22:16 -0500

    Initial commit

---
+++ json_fetcher.rb
@@ -0,0 +1,44 @@
+require 'net/http'
+require 'json'
+require 'time'
+
+def update_currency_rates
+  begin
+    # API URL
+    url = 'https://api.exchangerate-api.com/v4/latest/USD'
+
+    # Fetch data from the API
+    uri = URI(url)
+    response = Net::HTTP.get(uri)
+
+    # Parse the JSON response
+    data = JSON.parse(response)
+
+    # Format time_last_updated to MM/DD/YYYY HH:MM:SS UTC
+    formatted_time = Time.at(data['time_last_updated']).utc.strftime('%m/%d/%Y %H:%M:%S UTC')
+
+    # Clean the JSON by removing unnecessary fields and formatting time
+    cleaned_data = {
+      'base' => data['base'],
+      'time' => formatted_time,
+      'rates' => data['rates']
+    }
+
+    # Save the cleaned JSON to a file
+    File.open('currencyRates.json', 'w') do |file|
+      file.write(JSON.pretty_generate(cleaned_data))
+    end
+
+    puts 'currencyRates.json has been successfully updated!'
+
+  rescue JSON::ParserError => e
+    puts "Error: Failed to parse JSON response. Details: #{e.message}"
+  rescue Errno::ENOENT => e
+    puts "Error: File operation failed. Details: #{e.message}"
+  rescue StandardError => e
+    puts "An unexpected error occurred: #{e.message}"
+  end
+end
+
+# Call the function
+update_currency_rates
